f4a62bc99d5cdc4728df539a44bbae8ee1c6435a
alexanderccantor-cell <alexanderccantor@gmail.com>
2026-06-10 08:20:49 -0500

    Update main.tsx
    Fix main - remove ClerkProvider to fix black screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ClerkProvider } from '@clerk/clerk-react';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || '';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </ClerkProvider>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
